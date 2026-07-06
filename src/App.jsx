0bdca920dcad0b82a07d104ee4d8fcb6c59fbd66
Deltalay <88176924+Deltalay@users.noreply.github.com>
2026-07-06 14:26:06 +0700

    Remove unused code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
 import NavBar from '@/components/NavBar.jsx';
 import { Outlet} from 'react-router'
-import Home from '@/routes/Home.jsx'
 import Footer from '@/components/Footer.jsx'
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(true);
-  function change_bg() {
-    darkMode ? setDarkMode(false) : setDarkMode(true);
-  }
   return (
     <>
     <NavBar/>
