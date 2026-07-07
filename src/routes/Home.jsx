af30b6f2c601f77112886cebb34b9b6d7d604580
Deltalay <88176924+Deltalay@users.noreply.github.com>
2026-07-07 15:13:47 +0700

    Use Canvas instead of css

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,38 @@
 import { NavLink } from "react-router";
+import { useRef } from "react";
 import "../Circle.css";
+import { useEffect } from "react";
 export default function Home() {
+    const canvasRef = useRef(null);
+    function canvasImage() {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        const gradient = ctx.createRadialGradient(
+            300,
+            250,
+            0, // Inner circle (center, radius 0)
+            300,
+            250,
+            200, // Outer circle (center, radius 200)
+        );
+
+        // Add color stops
+        gradient.addColorStop(0.05, "#FFA10A"); // 5%
+        gradient.addColorStop(0.35, "#FFBB00"); // 35%
+        gradient.addColorStop(0.81, "#EE9300"); // 81%
+
+        ctx.beginPath();
+        ctx.arc(250, 250, 200, 0, 2 * Math.PI);
+        ctx.fillStyle = gradient;
+        ctx.fill();
+        ctx.strokeStyle = 'transparent';
+        ctx.stroke();
+
+        console.log(ctx);
+    }
+    useEffect(() => {
+        canvasImage();
+    }, [canvasRef]);
     return (
         <div>
             <div className="h-[calc(100vh-72.8px)]  justify-center lg:justify-normal items-center flex">
@@ -29,7 +61,7 @@ export default function Home() {
                 </div>
             </div>
             <div className="absolute top-[calc(50%-250px)] right-0 w-[300px] h-[500px] overflow-hidden">
-                <div className="outside__circle"></div>
+                <canvas ref={canvasRef} width="300px" height={"500px"} />
             </div>
         </div>
     );
