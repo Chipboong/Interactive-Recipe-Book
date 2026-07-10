168ccb3d11a760f9cc16f5dfca7947dd82580c3f
Chipboong <chipboongte132@gmail.com>
2026-07-10 21:26:13 +0700

    configure hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { RecipeProvider } from '@/context/RecipeContext'
+import {useEffect} from 'react'
 import { Outlet } from 'react-router'
 import Navbar from '@/components/NavBar'
 import Footer from '@/components/Footer'
@@ -15,6 +16,16 @@ import Footer from '@/components/Footer'
 // }
 
 export default function App() {
+    useEffect(() => {
+    // If the server path is anything other than the root "/"
+    if (window.location.pathname !== '/') {
+      // Grab what they mistakenly typed
+      const mistakenPath = window.location.pathname;
+      
+      // Force the browser to reset the URL to the root, and move their path AFTER the hash
+      window.location.replace('/#' + mistakenPath);
+    }
+  }, []);
     return (
         <RecipeProvider>
             <Navbar />
