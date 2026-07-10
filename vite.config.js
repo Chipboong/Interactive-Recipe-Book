99de3b7499c71108064efe21d3758a08f3fb1825
Deltalay <88176924+Deltalay@users.noreply.github.com>
2026-07-10 22:07:01 +0700

    Revert "test"
    This reverts commit b17e9bfa7a4406ad6d03c1a25cf6761800eb0881.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Interactive-Recipe-Book/",
+  base: "/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
